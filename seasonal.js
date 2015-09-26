b6bc4540ec17a4d3e0b7378eda37a29c84071385
Jason Heppler <jason.heppler@gmail.com>
2015-09-25 17:11:18 -0700

    Working on highlight features.

--- seasonal.js
+++ seasonal.js
@@ -1,5 +1,5 @@
 var margin_timeline = {top: 20, right: 20, bottom: 30, left: 60},
-    width_timeline = 860 - margin_timeline.left - margin_timeline.right,
+    width_timeline = 1010 - margin_timeline.left - margin_timeline.right,
     height_timeline = 90 - margin_timeline.top - margin_timeline.bottom;
 
 // var formatPercent = d3.format(".0%");
@@ -7,6 +7,8 @@ var margin_timeline = {top: 20, right: 20, bottom: 30, left: 60},
 // var color = d3.scale.linear()
 //   .range(["#ca0020","#f4a582","#f7f7f7","#92c5de","#0571b0"])
 //   .domain([0,0.2,0.4,0.6,0.8]);
+var y0 = d3.scale.ordinal()
+    .rangeRoundBands([height_timeline, 0], .2);
 
 var x_timeline = d3.scale.ordinal()
   .rangeRoundBands([0, width_timeline], 0.1);
@@ -31,6 +33,8 @@ d3.csv("data/foods_t.csv", type, function(data) {
       .key(function(d) { return d.season; })
       .entries(data);
 
+  y0.domain(foods.map(function(d) { return d.key; }));
+
   // Compute the minimum and maximum year and percent across symbols.
   x_timeline.domain(data.map(function(d) { return d.year; }));
   y_timeline.domain([0, d3.max(foods, function(s) { return s.values[0].percent; })]);
@@ -44,15 +48,15 @@ d3.csv("data/foods_t.csv", type, function(data) {
   .append("g")
     .attr("transform", "translate(" + margin_timeline.left + "," + margin_timeline.top + ")");
 
-  svg_timeline.append("g")
+  svg_timeline.filter(function(d, i) { return !i; }).append("g")
       .attr("class", "x axis")
-      .attr("transform", "translate(0," + height_timeline + ")")
+      .attr("transform", "translate(0," + y0.rangeBand() + ")")
       .call(xAxis_timeline)
     .selectAll("text")
       .attr("y", 0)
       .attr("x", 9)
       .attr("dy", ".35em")
-      .attr("transform", "rotate(90)")
+      .attr("transform", "rotate(65)")
       .style("text-anchor", "start");
 
   // svg_timeline.append("g")
